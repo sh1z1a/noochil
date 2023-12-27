f289936169b694973240c07430f81687204548ac
sh1z1a <h4sn44ziz4h@gmail.com>
2023-12-27 15:47:41 +0700

    push resource dan update routes "web"

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('partials.landingPages.main');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/', function () {
-    return view('welcome');
-});
